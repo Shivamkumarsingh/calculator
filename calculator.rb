9b7c57497afd41145aa6fa07eb817694d58e6579
Shivamkumarsingh <shivam1995k@gmail.com>
2024-12-18 20:30:47 +0530

    Handled single number as argument

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,5 @@
 def add(numbers)
   return 0 if numbers.empty?
+  numbers.to_i
 end
+
--- calculator_spec.rb
+++ calculator_spec.rb
@@ -7,4 +7,9 @@ class StringCalculatorTest < Minitest::Test
   def test_empty_string_returns_zero
     assert_equal 0, add("")
   end
+
+  # Handle single number"
+  def test_single_number_returns_number
+    assert_equal 5, add("5")
+  end
 end
